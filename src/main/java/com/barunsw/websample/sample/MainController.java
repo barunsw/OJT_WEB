f781176ccc84b5ba60fc56227ea5e0d4d6918298
김윤제 <forteas2003@hanmail.net>
2019-06-13 13:52:39 +0900

    [김윤제] 18일차 Spring 환경설정

--- src/main/java/com/barunsw/websample/sample/MainController.java
+++ src/main/java/com/barunsw/websample/sample/MainController.java
@@ -1,13 +1,18 @@
 package com.barunsw.websample.sample;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
+	private static final Logger LOGGER = LogManager.getLogger(MainController.class);
+
 	@RequestMapping(value="", method=RequestMethod.GET)
 	public String mainView() {
+		LOGGER.debug("Main");
 		return "main/main";
 	}
 }
---
+++ src/main/java/com/barunsw/websample/sample/SampleDao.java
@@ -0,0 +1,8 @@
+package com.barunsw.websample.sample;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SampleDao {
+
+}
