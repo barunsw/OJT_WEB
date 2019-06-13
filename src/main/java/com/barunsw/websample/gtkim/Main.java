c8f5205c5491196fbb5cacaa70ecf60fd9fb5151
김균태 <user@DESKTOP-636TPMV>
2019-06-14 08:25:21 +0900

    [김균태] 이름 중복 수정

--- src/main/java/com/barunsw/websample/gtkim/Main.java
+++ src/main/java/com/barunsw/websample/gtkim/Main.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class MainController {
+public class Main {
 	
-	@RequestMapping(value="", method=RequestMethod.GET)
+	@RequestMapping(value="/gtkim", method=RequestMethod.GET)
 	public String mainView() {
 		return "main/main";
 	}
